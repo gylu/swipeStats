1d2c29417e586022b6835f23a04463c83850fd08
Dan Torczynski <dtorczynski@gmail.com>
2015-07-05 20:09:58 -0700

    Removed log in /new_proposition route on backend

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ app.get('/init', function(request,response){
 });
 
 app.post('/new_proposition', jsonencode, function(request,response){
-    console.log(request.body);
     client.hkeys('new_propositions',function(error,newProps){
       var newPropNum = newProps.length;
       var newProp = 'newProp' + (newPropNum + 1);
