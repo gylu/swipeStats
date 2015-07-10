9df53ca21baaf48111974eebbfce81215c901602
George <gylu88@gmail.com>
2015-07-09 22:42:53 -0700

    commit before switching branches

--- app/app.js
+++ app/app.js
@@ -52,7 +52,7 @@ app.controller('mainController', function($scope,$stateParams,$state, $rootScope
    $scope.init();
 
 	$scope.propositionClicked = function(choice){
-		$rootScope.style = styles[choice];
+
 		console.log("for proposition: "+$scope.propositionID + ", user selected: " +choice);
         // ajax call to server to post
         /*var propData = {sessionID: $scope.sessionID, proposition: $scope.propositionID, choice: choice};
@@ -74,28 +74,7 @@ app.controller('mainController', function($scope,$stateParams,$state, $rootScope
         // if propsShown.length % 5 == 0, get prediction from server and show
 	}
 	
-	//stuff for swiping using angular's ng-touch
-	$scope.direction = 'left';
-	$scope.currentIndex = 0;
 
-	$scope.setCurrentSlideIndex = function (index) {
-		$scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-		$scope.currentIndex = index;
-	};
-
-	$scope.isCurrentSlideIndex = function (index) {
-		return $scope.currentIndex === index;
-	};
-
-	$scope.prevSlide = function () {
-		$scope.direction = 'left';
-		$scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-	};
-
-	$scope.nextSlide = function () {
-		$scope.direction = 'right';
-		$scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-	};
 	
 	$scope.$parent.keyPressed = function(event){
 	    console.log(event.keyCode + " In child scope.");
@@ -113,13 +92,7 @@ app.controller('mainController', function($scope,$stateParams,$state, $rootScope
     	}
     }
 
-  var styles = {
-    // appear from right
-    0: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(100%,0,0)  }  .enter-setup.enter-start {   position:absolute;  -webkit-transform:translate3d(0,0,0)}  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(-100%,0,0) };',
-    // appear from left
-    1: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all; -webkit-transform:translate3d(-100%,0,0)}  .enter-setup.enter-start {   position:absolute;   -webkit-transform:translate3d(0,0,0) }  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;  -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(100%,0,0) };'
-  };
-  $rootScope.style=styles[0];
+
 });
     
 app.controller('submitController', function($scope, $stateParams,$state) {
@@ -141,7 +114,7 @@ app.controller('submitController', function($scope, $stateParams,$state) {
 
 
 app.controller('BodyController', function($scope, $stateParams,$state) {
-	$scope.$parent.keyPressed = function(event){
+	$scope.keyPressed = function(event){
 	    console.log(event.keyCode + " In parent scope");
 	    if (event.keyCode == 38)
 	        console.log("up arrow");
@@ -158,42 +131,6 @@ app.controller('BodyController', function($scope, $stateParams,$state) {
     }
 });
 
-app.animation('.slide-animation', function () {
-	return {
-		addClass: function (element, className, done) {
-			var scope = element.scope();
-
-			if (className == 'ng-hide') {
-				var finishPoint = element.parent().width();
-				if(scope.direction !== 'right') {
-					finishPoint = -finishPoint;
-				}
-				TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
-			}
-			else {
-				done();
-			}
-		},
-		removeClass: function (element, className, done) {
-			var scope = element.scope();
-
-			if (className == 'ng-hide') {
-				element.removeClass('ng-hide');
-
-				var startPoint = element.parent().width();
-				if(scope.direction === 'right') {
-					startPoint = -startPoint;
-				}
-
-				TweenMax.set(element, { left: startPoint });
-				TweenMax.to(element, 0.5, {left: 0, onComplete: done });
-			}
-			else {
-				done();
-			}
-		}
-	};
-});	
 		
  })(); 
 //reason why the whole thing is wrapped in parenthesis for javascript to work
